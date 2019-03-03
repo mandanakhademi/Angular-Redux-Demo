1a9da33ed1b0883f74e6318f006ab299fb27d9f7
Mandana <mandanakhademi88@gmail.com>
2019-03-03 01:41:16 +0000

    refactor code to avoid state mutation

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,5 @@
 
-
+import { tassign } from 'tassign';
 import { INCREMENT } from './actions';
 import { combineReducers, Reducer } from 'redux';
 
@@ -14,7 +14,8 @@ export const INITIAL_STATE: IAppState = {
 
 export function rootReducer(state: IAppState, action): IAppState {
     switch (action.type) {
-        case INCREMENT: return { counter: state.counter + 1 };
+        case INCREMENT:
+            return tassign( state, { counter: state.counter + 1 });
     }
     return state;
 }
